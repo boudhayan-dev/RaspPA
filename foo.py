cffab926e57752b86c1c6007761286717ccc1a51
Boudhayan Dev <boudhayan-dev@users.noreply.github.com>
2017-11-20 23:18:21 +0530

    s

--- foo.py
+++ foo.py
@@ -2,25 +2,18 @@
 import dropbox,os,shutil,time
 from audioSplitter import audio_convert
 from sppech import speech_to_text
-dbx=dropbox.Dropbox('jj41foGWJMAAAAAAAAAAmFaIj31Nkz_Rbtezie-WDypRLXIIHY3E4i9rpBexdjGD')
+dbx=dropbox.Dropbox('YOUR_DROPBOX_APP_KEY')
 
 
 while(1):
-	dbx.users_get_current_account()
+	dbx.users_get_current_account() # getcurrent account details.
+	files=dbx.files_list_folder('').entries # find all files/subfolders inside the folder .
+	files=[i.name for i in files if (i.name).endswith("mp3")==True] # filter the mp3 files.
 
-
-	files=dbx.files_list_folder('').entries
-	files=[i.name for i in files if (i.name).endswith("mp3")==True]
-
-	modified_status=False
+	modified_status=False # Flag to check if files have changed since last time.
 	new_files=[] #stores info about new files downloaded
-
-
-
-
-
+	
 	for i in files:
-
 		with open("Records.txt","r+") as f:
 			ExistingRecords=f.readlines()
 			if not i+"\n" in ExistingRecords:
